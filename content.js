a95f4cf358746af6cc74b0be947559fbed6d69df
lharries <github@lukeharries.me>
2018-01-20 14:40:38 +0000

    added gitignore

--- content.js
+++ content.js
@@ -1,11 +1,29 @@
-var images = document.getElementsByTagName('img'); 
-var srcList = [];
-// get all the images
-for(var i = 0; i < images.length; i++) {
-    srcList.push(images[i].src);
+function getImageCaptions(dataToSend) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            console.log(alt, xhr.responseText);
+            // get the array of captions from the response text
+        }
+    }
+    xhr.open('POST', 'https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699', true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xhr.send(dataToSend);
 }
 
-for(var i = 0; i < images.length; i++) {
-    images[i].alt = "Test";
+function getImages() {
+    var images = document.getElementsByTagName('img');
+    var srcList = [];
+    // get all the images
+    for (var i = 0; i < images.length; i++) {
+        if ((!images[i].alt ||  !images[i].alt.length) && images[i].src.length) {
+            console.log(images[i])
+            srcList.push({url: images[i].src, alt: null});
+        }
+    }
+    return srcList;
 }
 
+var images = getImages();
+console.log(images);
+// addImageCaption();
