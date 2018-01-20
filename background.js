dd49b519b8b2fd7427cad2f0e9a3bec1ec531e9f
mjs114 <mateuszjstaniszewski@gmail.com>
2018-01-20 17:22:28 +0000

    adding refresh functionality

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 // background.js
 
+
+
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
--- content.js
+++ content.js
@@ -1,19 +1,28 @@
 
 // content.js
-var firstHref = $("a[href^='http']").eq(0).attr("href");
 
-console.log(firstHref);
+function get_tags(){
+	var images = document.getElementsByTagName('img'); 
+	var srcList = [];
+	// get all the images
+	for(var i = 0; i < images.length; i++) {
+	    srcList.push(images[i].src);
+	}
 
-
-var images = document.getElementsByTagName('img'); 
-var srcList = [];
-// get all the images
-for(var i = 0; i < images.length; i++) {
-    srcList.push(images[i].src);
+	for(var i = 0; i < images.length; i++) {
+	    images[i].alt = "Test";
+	}
 }
 
-for(var i = 0; i < images.length; i++) {
-    images[i].alt = "Test";
-}
 
-console.log(srcList)
+get_tags()
+
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+    	get_tags()
+    }
+  }
+);
