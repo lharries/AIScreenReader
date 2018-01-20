c6298e1e1d50b068d4b1b60c0e1ce36eec23bbf1
lharries <github@lukeharries.me>
2018-01-20 13:28:08 +0000

    added loop through images

--- content.js
+++ content.js
@@ -1,1 +1,11 @@
-alert("Hello");
+var images = document.getElementsByTagName('img'); 
+var srcList = [];
+// get all the images
+for(var i = 0; i < images.length; i++) {
+    srcList.push(images[i].src);
+}
+
+for(var i = 0; i < images.length; i++) {
+    images[i].alt = "Test";
+}
+
